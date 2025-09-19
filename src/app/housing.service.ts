f10fd9373eec42ddd2c7f9e1853b6aa0637b3b85
webdevs-ke <bernardkatiku@jomcon.co.ke>
2025-09-19 12:23:47 +0300

    first

---
+++ src/app/housing.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  protected housingLocationList: HousingLocation[] = [];
+  
+  constructor() { }
+}
